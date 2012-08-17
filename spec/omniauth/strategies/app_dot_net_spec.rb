715fff573d320602f502880c07b7f0c08399437f
Jacques Crocker <railsjedi@gmail.com>
2012-08-16 17:45:59 -0700

    some basic specs

--- spec/omniauth/strategies/app_dot_net_spec.rb
+++ spec/omniauth/strategies/app_dot_net_spec.rb
@@ -1,7 +1,21 @@
 require 'spec_helper'
 
 describe OmniAuth::Strategies::AppDotNet do
-  it 'should do some testing' do
-    pending
+  subject do
+    OmniAuth::Strategies::AppDotNet.new({})
+  end
+
+  context "client options" do
+    it 'should have correct name' do
+      subject.options.name.should eq("appdotnet")
+    end
+
+    it 'should have correct site' do
+      subject.options.client_options.site.should eq('https://alpha.app.net/')
+    end
+
+    it 'should have correct authorize url' do
+      subject.options.client_options.authorize_url.should eq('https://alpha.app.net/oauth/authenticate')
+    end
   end
 end
